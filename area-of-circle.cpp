749c32f58683ab4b7916f4e41041ba1309a99445
RenX86 <galactusruler@gmail.com>
2024-09-25 18:25:13 +0530

    Update

---
+++ area-of-circle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main (){
+    float r;
+    cout<<"Enter radius: ";
+    cin>>r;
+    float a = 3.14*r*r;
+    cout<<"Area of circle is: "<<a<<"\n";
+
+return 0 ;
+}
