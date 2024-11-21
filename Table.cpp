7d2fda9bcc7aa4074616a47d230e7f3c6b6d5565
RenX86 <galactusruler@gmail.com>
2024-11-21 17:44:31 +0530

    x

---
+++ Table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int  n; 
+    cout<<"Enter The Number: ";
+    cin>>n;
+
+    for (int i = n; i<=10*n; i=i+n)
+    {
+       
+        cout<<i<<endl;
+
+    }
+}
