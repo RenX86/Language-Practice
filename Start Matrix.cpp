00726077b5ef9f15fcd30c0890f9586fcf2442e3
RenX86 <galactusruler@gmail.com>
2024-11-24 11:51:41 +0530

    Update

---
+++ Start Matrix.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m,n;
+    cout<<"Enter the number of rows: ";
+    cin>>m;
+    cout<<"Enter the number of cols: ";
+    cin>>n;
+    for ( int i = 1; i < m; i++)
+    {
+        for (int  j = 1; j < n; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+}
