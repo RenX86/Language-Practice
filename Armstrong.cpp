68994cf387e880f5a81f9fa143bef7f033f526f4
RenX86 <galactusruler@gmail.com>
2024-11-25 14:45:04 +0530

    Update

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int num, originalNum, remainder, sum = 0;
+int main(){
+    int n,orn,r,sum = 0;
 
-    // Input the number from the user
-    cout << "Enter the number to check: ";
-    cin >> num;
+    cout << "Enter the number: ";
+    cin >> n;
+    orn = n;
 
-    // Store the original number for comparison
-    originalNum = num;
-
-    // Calculate the sum of cubes of each digit
-    while (num != 0) {
-        remainder = num % 10;         // Get the last digit
-        sum += remainder * remainder * remainder;  // Add the cube of the digit to sum
-        num /= 10;                    // Remove the last digit
+    while (n != 0)
+    {
+        r = n%10;
+        sum += r*r*r;
+        n /= 10;
     }
+    
+    if (orn == sum) cout << "The number is Armstrong";
+    else cout<< "Number the is not armstrong";
 
-    // Check if the calculated sum matches the original number
-    if (originalNum == sum)
-        cout << originalNum << " is an Armstrong number." << endl;
-    else
-        cout << originalNum << " is not an Armstrong number." << endl;
-
-    return 0;
+return 0;
 }
