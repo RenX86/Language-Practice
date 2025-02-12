11973124612c24cd5c909d1591d45cdb2c24157a
RenX86 <galactusruler@gmail.com>
2025-02-12 17:01:47 +0530

    12-2-25

---
+++ Python/Ex-5-6.py
@@ -0,0 +1,12 @@
+print("What is your name?", end=' ')
+name = input()
+print("What is your Age?", end=' ')
+age = int(input())
+print("What is your size?", end=' ')
+size = int(input())
+
+print(f"Your name is {name}, your are {age} years old and your size is {size}.")
+
+# print(input("What is your name?. "), input("What is your Age? "), input("What is your size? "))
+
+# name = input("What is your name? "); age = input("What is your Age? "); size = input("What is your size? "); print(f"Your name is {name}, your age is {age} years old and your size is {size}.")
